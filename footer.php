a8de6ad77b2fdac02fbe03ed33eb748b2ad38d49
Daniel Lousa <dlousa92@gmail.com>
2019-01-14 22:40:13 -0500

    fix spacing

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 </div><!-- /.container -->
 
     <footer class="blog-footer">
-      <?php if ( is_active_sidebar( 'footer_copyright-text') ) { dynamic_sidebar( 'footer-copyright-text'); } ?>
+      <?php if ( is_active_sidebar( 'footer_copyright_text') ) { dynamic_sidebar( 'footer_copyright_text'); } ?>
     </footer>
     <?php wp_footer(); ?>
   </body>
