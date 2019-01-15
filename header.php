cfa99645d53893230f2d49311fd56df909925f38
Daniel Lousa <dlousa92@gmail.com>
2019-01-14 22:24:53 -0500

    proper spacing

--- header.php
+++ header.php
@@ -28,9 +28,9 @@
 <div class="container">
 
  <div class="blog-header">
- <h1 class="blog-title"><?php bloginfo( 'name' ); ?></h1>
- <?php $description = get_bloginfo( 'description', 'display' ); ?>
- <?php if($description) { ?><p class='lead blog-description'><?php echo $description ?></p><?php } ?>
+    <h1 class="blog-title"><?php bloginfo( 'name' ); ?></h1>
+    <?php $description = get_bloginfo( 'description', 'display' ); ?>
+    <?php if($description) { ?><p class='lead blog-description'><?php echo $description ?></p><?php } ?>
  </div>
 
  <div class="row">
